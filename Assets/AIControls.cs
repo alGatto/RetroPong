691ee7b6105c47658cfb1cbb323315348b0663aa
alGattuso <gattuso.al@gmail.com>
2018-04-13 23:21:59 +0200

    Init

---
+++ Assets/AIControls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIControls : MonoBehaviour {
+    public float speed = 10.0f;
+    public float boundY = 2.25f;
+
+    private GameObject ball;
+    private Vector2 ballPos;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    void Move(){
+        if (!ball)
+            ball = GameObject.FindGameObjectWithTag("Ball");
+        //Debug.Log("We've got the ball!");
+        //Debug.Log(ball.GetComponent<BallControl>().ballDirection);
+
+        if(ball.GetComponent<BallControl> ().ballDirection == Vector2.right){
+            //Debug.Log("Ball to right");
+            ballPos = ball.transform.localPosition;
+
+            if(transform.localPosition.y > -boundY && ballPos.y < transform.localPosition.y){
+                transform.localPosition += new Vector3(0, -speed * Time.deltaTime, 0);
+            }
+            if (transform.localPosition.y < boundY && ballPos.y > transform.localPosition.y)
+            {
+                transform.localPosition += new Vector3(0, speed * Time.deltaTime, 0);
+            }
+        }
+            
+    }
+}
---
+++ Assets/BallControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private Vector2 vel;
+
+    public Vector2 ballDirection;
+
+
+    public Vector2 forceAdd = new Vector2(20.0f, 0);
+
+    public AudioClip MusicClip;
+    public AudioSource source;
+    public float hitVol = 1.0f;
+
+    void GoBall()
+    {
+        float rand = Random.Range(0, 2);
+        if (rand < 1)
+        {
+            rb2d.AddForce(new Vector2(20, -15));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2);
+	}
+
+    void Awake()
+    {
+
+        source = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ResetBall()
+    {
+        vel = Vector2.zero;
+        rb2d.velocity = vel;
+        transform.position = Vector2.zero;
+    }
+
+    void RestartGame()
+    {
+        ResetBall();
+        Invoke("GoBall", 1);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.CompareTag("Player"))
+        {
+            source.Play();
+            //vel.x = rb2d.velocity.x;
+            //vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);
+            vel.x = (rb2d.velocity.x * 2.0f) + (coll.collider.attachedRigidbody.velocity.x * 3.0f);
+            vel.y = rb2d.velocity.y;
+            rb2d.velocity = vel;
+            //rb2d.AddForce(forceAdd);
+        }
+    }
+}
